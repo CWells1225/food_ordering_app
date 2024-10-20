4567e51c5e5125cfe2cba9cdb1da52af21f0d508
CWells1225 <ebecochuba@yahoo.com>
2024-10-20 15:09:31 -0400

    adding category to form

--- src/components/resources/item.jsx
+++ src/components/resources/item.jsx
@@ -12,6 +12,13 @@ import { List,
     ImageInput, 
     ImageField, 
     NumberField,  
+    SimpleFormIterator,
+    ArrayInput,
+    BooleanInput,
+    ReferenceField,
+    ReferenceInput, 
+    SelectInput, 
+    ChipField,
 } from 'react-admin';
 
 import { BiFoodMenu } from "react-icons/bi";
@@ -22,9 +29,26 @@ return (
         <ImageInput source='image' label='Image'>
         <ImageField source='src' title='title' />
         </ImageInput>
+        <ReferenceInput source='category' reference='category'>
+        <SelectInput optionText='title' validate={[required()]} fullWidth />
+        </ReferenceInput>
         <TextInput source='label' validate={[required()]} fullWidth />
         <NumberInput source='price' validate={[required(), number()]} fullWidth />
         <TextInput source='description' fullWidth />
+        <ArrayInput source='variants'>
+            <SimpleFormIterator fullWidth>
+                <TextInput source='type' helperText={false} fullWidth/>
+                <ArrayInput source='choices'>
+            <SimpleFormIterator fullWidth>
+                <TextInput source='label' />
+                <NumberInput source='price' defaultValue={0}/>
+            </SimpleFormIterator>
+                </ArrayInput>
+                <BooleanInput source='allowMultiple' helperText={false} fullWidth />
+                <BooleanInput source='isRequired' helperText={false} fullWidth />
+            </SimpleFormIterator>
+        </ArrayInput>
+
     </SimpleForm>
 );
 }    
@@ -45,6 +69,9 @@ const ItemList = () => (
     <Datagrid rowClick='edit'>
         <ImageField source='image.src' label='Image'/>
         <TextField source='label' />
+        <ReferenceField source='category' reference='category'>
+            <ChipField source='title' />
+        </ReferenceField>
         <NumberField source='price' />
         <TextField source='description' />
         <DateField source='createdate' showTime label='Created At'/>
